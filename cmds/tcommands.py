38bca895a869d93a7a3e8eefafb0df0a35f18aeb
not-a-java-dev <68130396+not-a-java-dev@users.noreply.github.com>
2025-01-28 11:24:09 -0300

    change command prefix to t!!

--- cmds/tcommands.py
+++ cmds/tcommands.py
@@ -14,26 +14,27 @@ class TCommands(commands.Cog):
             return
 
         # no real use, stays here because cool
-        if message.content == "!a":
+        if message.content == "t!!a":
             await message.channel.send(f"Total: {self.client.index} | Minutes: {round(self.client.seconds/60,3)} | Result: {round((self.client.index*60)/self.client.seconds,3)}")
             return
 
         is_owner = await self.client.is_owner(message.author)
         # sync the commands
-        if message.content == "!tsync" and is_owner: 
-            self.client.logger.info("!tsync ran")
+        if message.content == "t!!sync" and is_owner:
+            self.client.logger.info("sync ran")
             await self.client.tree.sync()
             await message.channel.send("synced.")
             return
 
         # reload cogs
-        if message.content == "!creload" and is_owner: 
-            self.client.logger.info("!creload ran")
+        if message.content == "t!!creload" and is_owner:
+            self.client.logger.info("creload ran")
             for cog in self.client.cmds_cogs:
                 await self.client.reload_extension(f"cmds.{cog}")
                 self.client.logger.debug(f"reloaded cmds.{cog}")
 
             await message.channel.send("reloaded.")
+            return
 
         # if "error occured" in message.content.lower():
         #    await message.channel.send("I have my suspicions that `An error occured`")
